71442cbae0c4fdb2abcad8dea784c61abc5ba53d
Ivan Popov <116061712+RoshiSecOps@users.noreply.github.com>
2026-04-13 08:27:49 -0700

    Feature: Created add and get cache entry methods that belong to Cache struct and add/get cache entries.

--- internal/pokecache/cache.go
+++ internal/pokecache/cache.go
@@ -16,3 +16,26 @@ type cacheEntry struct {
 	createdAt time.Time
 	val       []byte
 }
+
+/*
+func NewCache(interval time.Duration) {
+
+}*/
+
+func (c Cache) Add(key string, val []byte) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	newEntry := cacheEntry{
+		createdAt: time.Now(),
+		val:       val,
+	}
+	c.Entries[key] = newEntry
+}
+
+func (c Cache) Get(key string) ([]byte, bool) {
+	entry, ok := c.Entries[key]
+	if !ok {
+		return nil, false
+	}
+	return entry.val, true
+}
