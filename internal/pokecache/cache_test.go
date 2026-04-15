74f4d8b24e2a0545ed85ffc29648526a4ba83b0d
Ivan Popov <116061712+RoshiSecOps@users.noreply.github.com>
2026-04-15 02:02:45 -0700

    Chore: Created test cases for pokecache package.

--- internal/pokecache/cache_test.go
+++ internal/pokecache/cache_test.go
@@ -1,3 +1,27 @@
 package pokecache
 
+import (
+	"testing"
+	"time"
+)
+
 // Test file for pokecache package
+
+func TestGet(t *testing.T) {
+	newCache := NewCache(3 * time.Second)
+	newCache.Add("test123", []byte("testdata"))
+	val, present := newCache.Get("test123")
+	if !present || string(val) != "testdata" {
+		t.Errorf("Added test123 entry with \"testdata\" value, but received %v", val)
+	}
+}
+
+func TestReap(t *testing.T) {
+	newCache := NewCache(3 * time.Second)
+	newCache.Add("test123", nil)
+	time.Sleep(4 * time.Second)
+	_, present := newCache.Get("test123")
+	if present {
+		t.Errorf("Entry should be deleted, but is present.")
+	}
+}
