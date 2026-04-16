ba29f656e228be34605cfc0d3a5e7bf72518e69d
Ivan Popov <116061712+RoshiSecOps@users.noreply.github.com>
2026-04-16 02:48:11 -0700

    Feature: Created GetPokemonStats function and Pokemon Struct to hold data.

--- internal/pokeapi/client.go
+++ internal/pokeapi/client.go
@@ -30,6 +30,44 @@ type LocationArea struct {
 	} `json:"pokemon_encounters"`
 }
 
+type Pokemon struct {
+	Name           string `json:"name"`
+	BaseExperience int    `json:"base_experience"`
+	Height         int    `json:"height"`
+	Weight         int    `json:"weight"`
+	Stats          []struct {
+		BaseStat int `json:"base_stat"`
+		Stat     struct {
+			Name string `json:"name"`
+		} `json:"stat"`
+	} `json:"stats"`
+	Types []struct {
+		Type struct {
+			Name string `json:"name"`
+		} `json:"type"`
+	} `json:"types"`
+}
+
+func GetPokemonStats(name string, c *pokecache.Cache) (pokemon Pokemon, err error) {
+	var newPokemon Pokemon
+	url := "https://pokeapi.co/api/v2/pokemon/"
+	finalUrl := url + name
+	res, err := http.Get(finalUrl)
+	if err != nil {
+		return newPokemon, err
+	}
+	defer res.Body.Close()
+	value, err := io.ReadAll(res.Body)
+	if err != nil {
+		return newPokemon, err
+	}
+	if err := json.Unmarshal(value, &newPokemon); err != nil {
+		fmt.Println("Error decoding response body.")
+		return newPokemon, err
+	}
+	return newPokemon, nil
+}
+
 func GetPokemons(location string, c *pokecache.Cache) (err error) {
 	url := "https://pokeapi.co/api/v2/location-area/"
 	finalurl := url + location
